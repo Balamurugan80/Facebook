b5fbc13fcb961cfafce6788a04d54f8d076a6634
Balamurugan80 <balacse93@gmail.com>
2022-10-17 14:43:08 +0530

    First Time code pushing

---
+++ src/test/java/Facebook.java
@@ -0,0 +1,15 @@
+
+public class Facebook {
+
+	
+	public void facebook1() {
+System.out.println("Launch url");
+	}
+
+	public void facebook2() {
+System.out.println("Login page Displayed");
+	}
+	public void facebook3() {
+System.out.println("User enter user Id");
+	}
+}
